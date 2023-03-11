e3339fe48187e321f989586774df3369747be1a1
lindani <panilindani02@gmail.com>
2023-03-11 15:52:48 +0200

    Setup django server and schema

---
+++ server/myapp/schema.py
@@ -0,0 +1,30 @@
+import graphene
+import requests
+
+
+class Person(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    height = graphene.String()
+    mass = graphene.String()
+    gender = graphene.String()
+    homeworld = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    all_people = graphene.List(Person)
+
+    def resolve_all_people(self, info):
+        response = requests.get('https://swapi.dev/api/people/')
+        people = response.json()
+        return [Person(id=person['id'],
+                       name=person['name'],
+                       height=person['height'],
+                       mass=person['mass'],
+                       gender=person['gender'],
+                       homeworld=person['homeworld']) for person in people]
+
+
+schema = graphene.Schema(query=Query)
+result = schema.execute()
+print(result)
