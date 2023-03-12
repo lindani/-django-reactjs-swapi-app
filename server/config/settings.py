ceed6313b5466c31e17de1022471a6745729cd59
lindani <panilindani02@gmail.com>
2023-03-12 11:34:23 +0200

    Install and add django cors settings

--- server/config/settings.py
+++ server/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
 ]
 
 GRAPHENE = {
@@ -48,12 +49,16 @@ GRAPHENE = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000',]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', ]
 
 ROOT_URLCONF = 'config.urls'
 
